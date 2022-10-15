239406660c282e5580715704b0ead70c4c10e661
Mateusz Warzecha <warzecha.mati@gmail.com>
2022-10-15 15:35:28 +0200

    rng guesser done

--- test1/src/Main.java
+++ test1/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Random random = new Random();
+        int numberToGuess = random.nextInt(100) + 1;
+        Scanner scanner = new Scanner(System.in);
+        boolean wasNumberGuessed = false;
+
+        while(!wasNumberGuessed){
+            System.out.println("podaj liczbe: ");
+            int number = scanner.nextInt();
+
+            if(number < numberToGuess){
+                System.out.println("Liczba jest za mala");
+            } else if (number> numberToGuess) {
+                System.out.println("Liczba jest za duza");
+            }else {
+                System.out.println("brawo, winek");
+                wasNumberGuessed=true;
+            }
+        }
     }
 }
