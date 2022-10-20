1ceccbcb189029bb1991039d8ec807d27e4dc53c
Mateusz Warzecha <warzecha.mati@gmail.com>
2022-10-20 12:10:08 +0200

    task 4 done

--- LAB_2/task_1/src/Main.java
+++ LAB_2/task_1/src/Main.java
@@ -1,15 +1,5 @@
-import java.util.Calendar;
-import java.util.Date;
-
 public class Main {
     public static void main(String[] args) {
-        Calendar c = Calendar.getInstance();
-        int i = c.get(Calendar.DAY_OF_WEEK);
 
-        if(i!=1 && i!=7) {
-            i = (i + 6) % 7;
-            int daysToWeekend = 5 - i;
-            System.out.println("Liczba dni do weekendu: " + daysToWeekend);
-        }
     }
 }
