ff17fdca397b666ff8efefb44f3b9ceb1b484aef
Mateusz Warzecha <warzecha.mati@gmail.com>
2022-10-27 12:52:21 +0200

    lab 3

--- LAB_3/task_1/src/TestClass.java
+++ LAB_3/task_1/src/TestClass.java
@@ -1,2 +1,15 @@
 public class TestClass {
+    /*
+    brak arg
+
+    current time milis -> system documenattion
+        priavte static long? = SYys.curr
+    pole statyczne z licznikiem
+        private static int counter  = 0
+    name =++counter;
+
+    przeciazenie metody to string
+    public str + str
+        return this.name + ". ["+time+"]"
+     */
 }
