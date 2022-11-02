82aa9679ce8fc02b186e812eff2207a6d57f2d92
Mateusz Warzecha <warzecha.mati@gmail.com>
2022-11-02 13:28:06 +0100

    renew

---
+++ OOP/src/LAB_2/CommandLineArguments.java
@@ -0,0 +1,8 @@
+public class CommandLineArguments {
+    public static void main(String[] arguments) {
+        System.out.println("args length= " + arguments.length);
+         for(int i = 0; i < arguments.length; i++) {
+                System.out.println((i+1)+". "+arguments[i]);
+         }
+    }
+}
---
+++ OOP/src/LAB_4/LineCounter.java
@@ -0,0 +1,7 @@
+package LAB_4;
+
+public class LineCounter {
+    public static void main(String[] args){
+        System.out.println("eki");
+    }
+}
