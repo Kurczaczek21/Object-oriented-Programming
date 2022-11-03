396220f6b9638408de4d91575960363c7d3d66d4
Mateusz Warzecha <warzecha.mati@gmail.com>
2022-11-03 12:45:08 +0100

    lab_4 halfly done

--- OOP/src/LAB_4/LineCounterPane.java
+++ OOP/src/LAB_4/LineCounterPane.java
@@ -1,31 +1,36 @@
 package LAB_4;
 
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class LineCounter {
+public class LineCounterPane {
     public static void main(String[] args) throws IOException {
-        if(args.length==0){
+        JFrame f = new JFrame();
+        String fileName = JOptionPane.showInputDialog(f, "Podaj nazwę pliku: ");
+        System.out.println(fileName);
+
+        if (fileName.length() == 0) {
             System.out.println("Brak argumentów programu.");
             System.exit(0);
         }
-        String fileName = args[0];
-        String fileLocation = "C:\\Users\\Mateusz\\IdeaProjects\\java college\\data\\"+fileName;
+        String fileLocation = "C:\\Users\\Mateusz\\IdeaProjects\\java college\\data\\" + fileName;
         BufferedReader reader = null;
 
-        try{
+        try {
             reader = new BufferedReader(new FileReader(fileLocation));
-        }catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
             System.exit(0);
         }
         int lines = 0;
-        while(reader.readLine()!=null){
+        while (reader.readLine() != null) {
             lines++;
         }
 
         System.out.println("Liczba wierszy w pliku " + fileName + " wynosi: " + lines);
+        System.exit(0);
     }
 }
