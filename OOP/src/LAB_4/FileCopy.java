d5d6723f723553988285b2b95d76b0146342441c
Mateusz Warzecha <warzecha.mati@gmail.com>
2022-11-06 14:11:13 +0100

    task 3 core done

--- OOP/src/LAB_4/FileCopy.java
+++ OOP/src/LAB_4/FileCopy.java
@@ -1,18 +1,31 @@
 package LAB_4;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class FileCopy {
     public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
-            System.out.println("Brak argumentów programu.");
-            System.out.println("Użycie: java FileCopy source_file target");
+        if (args.length != 2) {
+            System.out.println("Brak argumentów programu.\n" + "Użycie: java FileCopy <source file> <destanation>");
             System.exit(0);
         }
         String source = args[0];
         String destanation = args[1];
         String sourceFileLocation = "C:\\Users\\Mateusz\\IdeaProjects\\java college\\data\\" + source;
-        BufferedReader reader = null;
-    }
+        Path pathIn = Paths.get(sourceFileLocation);
+        String destanationFileLocation = "C:\\Users\\Mateusz\\IdeaProjects\\java college\\data\\" + destanation;
+        Path pathOut = Paths.get(destanationFileLocation);
+
+        try {
+            Files.copy(pathIn, pathOut, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        }
 }
