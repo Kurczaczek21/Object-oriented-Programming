29c0c00a41e4c5e5053b39377ef881452006a1db
Mateusz Warzecha <warzecha.mati@gmail.com>
2022-11-09 15:42:32 +0100

    task 5 started

--- OOP/src/LAB_4/FileAndURLCopy.java
+++ OOP/src/LAB_4/FileAndURLCopy.java
@@ -2,14 +2,15 @@ package LAB_4;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
-public class FileCopy {
+public class FileAndURLCopy {
     public static void main(String[] args) throws IOException {
-        if (args.length != 2) {
+        if (args.length > 2) {
             System.out.println("Brak argumentów programu.\n" + "Użycie: java FileCopy <source file> <destanation>");
             System.exit(0);
         }
@@ -20,6 +21,23 @@ public class FileCopy {
         Path pathOut = Paths.get(destanationFileLocation);
 
         File src = new File(sourceFileLocation), dst = new File(destanationFileLocation);
+
+        if (!isValidURL(args[0])) {
+            if (!src.exists()) {
+                System.out.println("Plik " + src.getName() + " nie istnieje.");
+                System.exit(1);
+            }
+
+            if (src.isDirectory()) {
+                System.out.println("Plik " + src.getName() + " jest katalogiem.");
+                System.exit(1);
+            }
+
+            if (!src.canRead()) {
+                System.out.println("Brak dostępu do pliku " + src.getName() + ".");
+                System.exit(1);
+            }
+        }
 
         if (!src.exists()) {
             System.out.println("Plik " + src.getName() + " nie istnieje.");
@@ -48,6 +66,15 @@ public class FileCopy {
             Files.copy(pathIn, pathOut, StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+    public static boolean isValidURL(String urlString) {
+        try {
+            URL url = new URL(urlString);
+            url.toURI();
+            return true;
+        } catch (Exception e) {
+            return false;
         }
     }
 }
