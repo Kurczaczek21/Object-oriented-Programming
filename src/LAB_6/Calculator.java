175bf479e79d481d738f3acd5e52eeb106eca628
Mateusz Warzecha PC <warzecha.mati@gmail.com>
2022-11-24 20:29:47 +0100

    ALL tests PASSED,working 100% added awesome comments

--- src/LAB_6/Calculator.java
+++ src/LAB_6/Calculator.java
@@ -1,52 +1,49 @@
 package LAB_6;
-
-import javax.swing.*;
-import java.awt.*;
-import java.util.HashMap;
-import java.util.Objects;
-
+                                                // Credits to Mateusz Warzecha
+import javax.swing.*;                           // github:
+import java.awt.*;                              // https://github.com/Kurczaczek21/
+import java.util.Objects;                       // feel free to copy <3
+                                                // i dont care, its just a code
 public class Calculator {
 
-    static double int_1 = Double.NaN;
-    static double int_2 = Double.NaN;
-    static double total = Double.NaN;
+    static double int_1 = Double.NaN;           // value of first variable
+    static double int_2 = Double.NaN;           // value of second variable
+    static double total = Double.NaN;           // value of result
 
-    static boolean lastInputWasEqual = false;
-    static boolean lastInputWasAction = true;
-    static boolean dividingByZero = false;
-    static boolean hasBeenSolved = true;
+    static boolean lastInputWasEqual = false;   // last key pressed: "="
+    static boolean lastInputWasAction = true;   // last key pressed: "+", "-", "*", "/"
+    static boolean dividingByZero = false;      // dividing by zero ERROR
+    static boolean hasBeenSolved = true;        // was "=" pressed AFTER inputing: "int_1" , "action" , "int_2"
+//                                                 and BEFORE pressing another new NUM key
+//                                                 ex:
+//                                                 1 + 2 - 6 = 3 - 6 =  -6 like on normal calculator
+//                                                 There is NO NEED to press "=" after every math operation
 
-    static String action = "none";
-    static String answer;
+    static String action = "none";              // current action: "none", "+", "-", "*", "/"
+    static String answer;                       // String value of total ( unnecessary, but I want it )
+
     public static void createAndShowGUI() {
 
-        // DISPLAY PNAEL
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridwidth = GridBagConstraints.REMAINDER;
-        JTextField field = new JTextField(12);
-        field.setFont(new Font("Calabri",Font.BOLD,24));
-        field.setHorizontalAlignment(JTextField.RIGHT);
-        field.setEditable( false );
-        field.setText("0");
+        // DISPLAY PANEL
+        GridBagConstraints gbc = new GridBagConstraints();           // Creates a GridBagConstraint object
+        gbc.gridwidth = GridBagConstraints.REMAINDER;                // Specifies that this component is the last component in its column or row.
+        JTextField field = new JTextField(12);               // Creates JTextField for 12 long characters
+        field.setFont(new Font("Calabi", Font.BOLD, 24));  // Font settings
+        field.setHorizontalAlignment(JTextField.RIGHT);              // Set text adjust to right side of screen
+        field.setEditable(false);                                 // Disabling Input from Keyboard
+        field.setText("0");                                         // Setting text on screen
 
 
         // BUTTON PANEL
-        JPanel panel = new JPanel();
-
-        panel.setPreferredSize(new Dimension(300,150));
-        panel.setBackground(Color.lightGray);
-        panel.setLayout(new GridLayout(4,4,6,6));
+        JPanel panel = new JPanel();                                    // Creates new JPanel
+        panel.setPreferredSize(new Dimension(300, 150));     // Set Panel size
+        panel.setBackground(Color.lightGray);                           // Set background colour
+        panel.setLayout(new GridLayout(4, 4, 6, 6));  // Set mesh layout for buttons on panel
 
 
         // BUTTON SETTINGS
-
-//        HashMap<Integer, JButton> numButtons = new HashMap<Integer, JButton>();
-//        for (int i=1;i<10;i++){
-//            numButtons.put(i,createButton(i, field));
-//        }
-
-        JButton button1 = new JButton("1");
-        numKeyAction(field, button1);
+        JButton button1 = new JButton("1");                     // creating numeric Buttons
+        numKeyAction(field, button1);                               // adding Button functionality via method
 
         JButton button2 = new JButton("2");
         numKeyAction(field, button2);
@@ -63,7 +60,6 @@ public class Calculator {
         JButton button6 = new JButton("6");
         numKeyAction(field, button6);
 
-
         JButton button7 = new JButton("7");
         numKeyAction(field, button7);
 
@@ -75,78 +71,77 @@ public class Calculator {
 
         JButton buttonZero = new JButton("0");
         buttonZero.addActionListener(e -> {
-            if(lastInputWasAction){
+            if (lastInputWasAction) {                     // check if it's used after action Button
                 field.setText("");
             }
-            if(lastInputWasEqual){
+            if (lastInputWasEqual) {                      // check if it's used after "="
                 field.setText("");
-                int_1 = Double.NaN;
-                int_2 = Double.NaN;
-                action = "none";
-                hasBeenSolved = true;
+                int_1 = Double.NaN;                     // setting default value of int_1
+                int_2 = Double.NaN;                     // setting default value of int_2
+                action = "none";                        // setting default value of action
+                hasBeenSolved = true;                   // setting default value of hasBeenSolved
             }
-            if(!field.getText().startsWith("0")) {
+            if (!field.getText().startsWith("0")) {      // if statement to PREVENT spamming zeros before int value
                 field.setText(field.getText().concat(buttonZero.getText()));
             }
-            lastInputWasAction = false;
-            lastInputWasEqual = false;
+            lastInputWasAction = false;                 // last input was number -> NOT action -> false
+            lastInputWasEqual = false;                  // last input was number -> NOT equal -> false
         });
-        buttonZero.setFocusable(false);
+        buttonZero.setFocusable(false);                 // removing this weird frame after button click
 
         // EQUATION BUTTON
-
-        JButton buttonEquals = new JButton("=");
-        buttonEquals.addActionListener(e -> {
-            if (lastInputWasEqual) {
-                int_1 = Double.parseDouble(field.getText());
+        JButton buttonEquals = new JButton("=");            // creating new Object
+        buttonEquals.addActionListener(e -> {                   // overriding method
+            if (lastInputWasEqual) {                            // Checking if "=" was used again to repeat last
+                int_1 = Double.parseDouble(field.getText());    // action and setting output as int_1
             } else {
                 int_2 = Double.parseDouble(field.getText());
             }
 
-            switch (action) {
+            switch (action) {                                   // choosing right math operation
                 case "+" -> total = int_1 + int_2;
                 case "-" -> total = int_1 - int_2;
                 case "*" -> total = int_1 * int_2;
                 case "/" -> {
-                    if (int_2 != 0) {
+                    if (int_2 != 0) {                           // dividing by zero check
                         total = int_1 / int_2;
                     } else {
                         dividingByZero = true;
                     }
                 }
-                case "none" -> total = Double.parseDouble(field.getText());
-            }
-            if ((total % 1) == 0) {
+                case "none" -> total = Double.parseDouble(field.getText());     // No action sign - just spamming "="
+            }                                                                   // button with 1 inputted value
+            if ((total % 1) == 0) {                             // removing .0 ending from Double if possible
                 answer = String.valueOf((int) total);
             } else {
                 answer = String.valueOf(total);
             }
-            if (!dividingByZero) {
+            if (!dividingByZero) {                              // dividing by zero error
                 field.setText(answer);
             } else {
                 field.setText("ERROR");
                 dividingByZero = false;
             }
-            lastInputWasEqual = true;
-            lastInputWasAction = false;
-            hasBeenSolved = true;
+            lastInputWasEqual = true;                           // last input was "=" -> equal -> true
+            lastInputWasAction = false;                         // last input was "=" -> NOT action -> false
+            hasBeenSolved = true;                               // last input was number -> task solved -> true
         });
-        buttonEquals.setFocusable(false);
+        buttonEquals.setFocusable(false);                       // removing this weird frame after button click
 
         JButton buttonClear = new JButton("C");
         buttonClear.addActionListener(e -> {
-            int_1 = Double.NaN;
-            int_2 = Double.NaN;
-            field.setText("0");
-            action = "none";
-            hasBeenSolved = true;
+            int_1 = Double.NaN;                                 // setting default value of int_1
+            int_2 = Double.NaN;                                 // setting default value of int_2
+            field.setText("0");                                 // setting default value of display
+            action = "none";                                    // setting default value of action
+            hasBeenSolved = true;                               // setting default value of hasBeenSolved
         });
-        buttonClear.setFocusable(false);
+        buttonClear.setFocusable(false);                        // removing this weird frame after button click
 
         // MATH OPERATION BUTTONS
 
-        JButton buttonAdd = new JButton("+");
-        mathKeyAction(field, buttonEquals, buttonAdd);
+        JButton buttonAdd = new JButton("+");               // Creating Math operation Buttons
+        mathKeyAction(field, buttonEquals, buttonAdd);           // adding Button functionality via method
 
         JButton buttonSubtract = new JButton("-");
         mathKeyAction(field, buttonEquals, buttonSubtract);
@@ -157,95 +152,84 @@ public class Calculator {
         JButton buttonDivision = new JButton("/");
         mathKeyAction(field, buttonEquals, buttonDivision);
 
-
-//        for (int index = 1; index <= 16; index++) {
-//            panel.add(button(index));
-//        }
-
+        // adding buttons to Panel Via weird InteliJ solution XD
         buttonToPanel(panel, button1, button2, button3, button4, button5, button6, buttonAdd, buttonSubtract);
         buttonToPanel(panel, button7, button8, button9, buttonZero, buttonEquals, buttonClear, buttonMultiplicative, buttonDivision);
 
-
         // MAIN FRAME
-        JFrame frame = new JFrame(" Calculator kurczaka");
-
-        ImageIcon image = new ImageIcon("kalkulator.png");
-        frame.setIconImage(image.getImage());
-
-        frame.setVisible(true);
-        frame.setSize(350,255);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.getContentPane().setBackground(Color.DARK_GRAY);
-        frame.setLayout(new FlowLayout());
-        frame.setLocationRelativeTo(null);
-        frame.add(field, gbc);
-        frame.add(panel);
+        JFrame frame = new JFrame(" Calculator kurczaka");      // setting name
+        ImageIcon image = new ImageIcon("kalkulator.png");  // upload icon
+        frame.setIconImage(image.getImage());                       // set icon
+        frame.setVisible(true);                                     // set visability of frame
+        frame.setSize(350, 255);                        // set size
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);       // closing frame will cause end of the program
+        frame.setResizable(false);                                  // cant resize window -> const size
+        frame.getContentPane().setBackground(Color.DARK_GRAY);      // set background colour
+        frame.setLayout(new FlowLayout());                          // set layout
+        frame.setLocationRelativeTo(null);                          // Display on center of the screen
+        frame.add(field, gbc);                                      // add text field
+        frame.add(panel);                                           // add button panel
     }
+
+    private static void numKeyAction(JTextField field, JButton button) {
+        button.addActionListener(evt -> {
+            if (lastInputWasAction) {                   // check if last input was action to clear
+                field.setText("");                      // text field before new input
+            }
+            if (lastInputWasEqual) {                    // check if last input was "=" to set default settings
+                field.setText("");                      // alternative:
+                int_1 = Double.NaN;                     // buttonZero.doClick but this looks better
+                int_2 = Double.NaN;
+                action = "none";
+                hasBeenSolved = true;
+            }
+            if (field.getText().equals("0")) {          // deleting 0 from beginning of number
+                field.setText("");                      // instead 04 -> 4
+            }
+            field.setText(field.getText().concat(button.getText()));    // insert your input at the end of text field
+            lastInputWasAction = false;                 // last input was number -> NOT action -> false
+            lastInputWasEqual = false;                  // last input was number -> NOT equal -> false
+        });
+        button.setFocusable(false);                     // removing this weird frame after button click
+    }
+
+    private static void mathKeyAction(JTextField field, JButton buttonEquals, JButton buttonAdd) {
+        buttonAdd.addActionListener(e -> {
+            if (Objects.equals(action, buttonAdd.getText()) && lastInputWasAction) {
+                return;                                         // check if same action button was pressed again
+            }                                                   // if TRUE do nothing -> return
+            if (!hasBeenSolved && !lastInputWasAction) {        // check if its situation 12 + 4 <CURRENTLY PRESSED BUTTON>
+                buttonEquals.doClick();                         // if TRUE do "=" task to get result and do another operation
+                hasBeenSolved = true;                           // previous task solved -> true
+            }
+            int_1 = Double.parseDouble(field.getText());        // set variable
+            action = buttonAdd.getText();                       // set current operation
+            if (!lastInputWasAction) {                          // check if last button pressed was operator
+                hasBeenSolved = false;                          // if FALSE task is NOT solved -> operator OVERRIDE
+            }
+            lastInputWasEqual = false;                          // last input was action -> NOT equal -> false
+            lastInputWasAction = true;                          // last input was action -> action -> true
+        });
+        buttonAdd.setFocusable(false);                          // removing this weird frame after button click
+    }
+
 
     private static void buttonToPanel(JPanel panel, JButton button1, JButton button2, JButton button3, JButton button4, JButton button5, JButton button6, JButton buttonAdd, JButton buttonSubtract) {
         panel.add(button1);
         panel.add(button2);
         panel.add(button3);
-        panel.add(buttonAdd);
-        panel.add(button4);
+        panel.add(buttonAdd);                                   // inteliJ solution - idk if its worth bcs it looks UGLY!
+        panel.add(button4);                                     // and benefit is code shorter by a few lines
         panel.add(button5);
         panel.add(button6);
         panel.add(buttonSubtract);
     }
 
-    private static void mathKeyAction(JTextField field, JButton buttonEquals, JButton buttonAdd) {
-        buttonAdd.addActionListener(e -> {
-            if(Objects.equals(action, buttonAdd.getText()) && lastInputWasAction){
-                return;
-            }
-            if (!hasBeenSolved && !lastInputWasAction) {
-                buttonEquals.doClick();
-                hasBeenSolved = true;
-            }
-            int_1 = Double.parseDouble(field.getText());
-            action = buttonAdd.getText();
-            if(!lastInputWasAction) {
-                hasBeenSolved = false;
-            }
-            lastInputWasEqual = false;
-            lastInputWasAction = true;
-        });
-        buttonAdd.setFocusable(false);
-    }
-
-    private static void numKeyAction(JTextField field, JButton button) {
-        button.addActionListener(evt -> {
-            if(lastInputWasAction){
-                field.setText("");
-            }
-            if(lastInputWasEqual){
-                field.setText("");
-                int_1 = Double.NaN;
-                int_2 = Double.NaN;
-                action = "none";
-                hasBeenSolved = true;
-            }
-            if(field.getText().equals("0")){
-                field.setText("");
-            }
-            field.setText(field.getText().concat(button.getText()));
-            lastInputWasAction = false;
-            lastInputWasEqual = false;
-        });
-        button.setFocusable(false);
-    }
-
-
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() { createAndShowGUI(); }
+            public void run() {
+                createAndShowGUI();                             // RUN calculator
+            }
         });
     }
-
-//    protected static JButton createButton(int index, JTextField txtField) {
-//        JButton btn = new JButton(String.valueOf(index));
-//        numKeyAction(txtField, btn);
-//        return btn;
-//    }
 }
